983d727f3bb7d29abbf8563a59950ac3fc62271f
Sanaullah Aslam <107422943+sanaullahaslam@users.noreply.github.com>
2023-12-14 12:01:32 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+            
+            <h1>
+              React App Template
+            </h1>
+         
+    </div>
+  );
+}
+
+export default App;
